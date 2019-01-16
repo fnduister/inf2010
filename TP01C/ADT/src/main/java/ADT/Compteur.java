0af12291716fa290b81c4ca31198c46fae93bb05
Fabrice Ndui <fnduister@gmail.com>
2019-01-16 13:52:10 -0500

    fini compteur

--- TP01C/ADT/src/main/java/ADT/Compteur.java
+++ TP01C/ADT/src/main/java/ADT/Compteur.java
@@ -8,30 +8,30 @@ public class Compteur implements Comparable<Compteur> {
      private int compteur = 0; // initialisation la valeur courant est 0
 
      public Compteur(String id) {
-          // completer
+          nom = id;
      }
 
      public void increment() {
-          // completer
+          compteur++;
      }
 
      public int score() {
-          // completer
+          return 0;
      }
 
      public String toString() {
-          // completer
+          return this.nom + " obtient un score de " + this.score() + " pour un compte de " + this.compteur;
      }
 
      public int compareTo(Compteur x) {
-          // completer
+          return this.score() - x.score();
      }
 
      private static Random random = new Random(10000);
 
      // Retourne un nombre entier aléatoire uniformément dans [0,n[
      public static int uniform(int n) {
-          // completer
+          return random.nextInt(n);
      }
 
      public static void main(String[] args) {
@@ -39,10 +39,23 @@ public class Compteur implements Comparable<Compteur> {
           int essais = 60000;
 
           // Creation n compteurs
+          Compteur compteurs[] = new Compteur[n];
+
+          for (int i = 0; i < n; i++) {
+               String name = "compteur" + i;
+               compteurs[i] = new Compteur(name);
+          }
 
           // incrémente les compteurs d'essais au hasard
+          for (int i = 0; i < essais; i++) {
+               int randomIndex = uniform(n);
+               compteurs[randomIndex].increment();
+          }
 
           // Affichage des resultat
+          for (Compteur compteur : compteurs) {
+               System.out.print(compteur.toString() + System.lineSeparator());
+          }
 
      }
 }
