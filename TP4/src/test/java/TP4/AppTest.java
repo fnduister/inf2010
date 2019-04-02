a71eb7c141fc291180f97bc846dc2fef2ee4a6b8
fabrice <fnduister@gmail.com>
2019-04-02 00:50:25 -0400

    probablement ecrase

---
+++ TP4/src/test/java/TP4/AppTest.java
@@ -0,0 +1,18 @@
+package TP4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
